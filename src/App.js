76475a834766901c6f2614fbe33b10efba2c3cf6
akanksha dinde <akankshadinde@gmail.com>
2024-02-20 16:28:53 +0530

    Digital Clock

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App(){
+      
+      let time = new Date().toLocaleTimeString();
+      const [currentTime,setCurrentTime] = useState(time);
+      const updateTime = () =>{
+        time = new Date().toLocaleTimeString();
+        setCurrentTime(time)
+      }
+
+      setInterval(updateTime,1000)
+   
+   return(
+    <>
+     <div className='body'>
+       <img src="https://images.unsplash.com/photo-1595950206452-00a91aa79ed6?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+     </div>
+       <h1>Digital Clock</h1>
+      <h3>{currentTime}</h3>
+    </>
+   );
 }
 
 export default App;
